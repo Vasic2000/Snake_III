b669d200784a1380fd5a3859265f43bf11c74826
vasic2000 <vasic2000@yandex.ru>
2023-07-26 15:00:50 +0300

    Buttons and move

---
+++ app/src/main/java/ru/vasic2000/snake_iii/Directions.kt
@@ -0,0 +1,5 @@
+package ru.vasic2000.snake_iii
+
+enum class Directions {
+    UP, RIGHT, DOWN, LEFT
+}
--- app/src/main/java/ru/vasic2000/snake_iii/MainActivity.kt
+++ app/src/main/java/ru/vasic2000/snake_iii/MainActivity.kt
@@ -3,8 +3,11 @@ package ru.vasic2000.snake_iii
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
+import ru.vasic2000.snake_iii.SnakeCore.SnakeCore
+import ru.vasic2000.snake_iii.SnakeCore.SnakeCore.StartTheGame
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,17 +21,42 @@ class MainActivity : AppCompatActivity() {
         val container = findViewById<LinearLayout>(R.id.activity_main)
         container.addView(head)
 
-        Thread(Runnable {
-            while (true) {
-                Thread.sleep(500)
-                runOnUiThread {
-                    (head.layoutParams as LinearLayout.LayoutParams).topMargin += 100
-                    container.removeView(head)
-                    container.addView(head)
-                }
+        val ivArrowUp = findViewById<ImageView>(R.id.ivArrowUp)
+        val ivArrowRight = findViewById<ImageView>(R.id.ivArrowRight)
+        val ivArrowDown = findViewById<ImageView>(R.id.ivArrowDown)
+        val ivArrowLeft = findViewById<ImageView>(R.id.ivArrowLeft)
+
+        ivArrowUp.setOnClickListener {
+            SnakeCore.nextMove = {move(Directions.UP, head, container)
             }
+        }
+        ivArrowRight.setOnClickListener {
+            SnakeCore.nextMove = {move(Directions.RIGHT, head, container)
+            }
+        }
+        ivArrowDown.setOnClickListener {
+            SnakeCore.nextMove = {move(Directions.DOWN, head, container)
+            }
+        }
+        ivArrowLeft.setOnClickListener {
+            SnakeCore.nextMove = {move(Directions.LEFT, head, container)
+            }
+        }
 
-        }).start()
+        StartTheGame()
 
+    }
+
+    fun move(directions : Directions, head: View, container: LinearLayout) {
+        when(directions) {
+            Directions.UP -> {(head.layoutParams as LinearLayout.LayoutParams).topMargin -=100}
+            Directions.RIGHT -> {(head.layoutParams as LinearLayout.LayoutParams).leftMargin +=100}
+            Directions.DOWN -> {(head.layoutParams as LinearLayout.LayoutParams).topMargin +=100}
+            Directions.LEFT -> {(head.layoutParams as LinearLayout.LayoutParams).leftMargin -=100}
+        }
+        runOnUiThread {
+            container.removeView(head)
+            container.addView(head)
+        }
     }
 }
